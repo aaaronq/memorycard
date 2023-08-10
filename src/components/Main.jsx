28361c96e2f81fb1bebffc1a8992515349b85912
aaaronq <aaronquigley@mail.com>
2023-08-10 01:19:34 +0100

    begin basic html/css

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+export default function Main() {
+    return (
+        <div className="main">
+            <h2> Pick a different character each time! </h2>
+            <div className="grid">
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+            </div>
+        </div>
+        );
+}
